99201c6fcc53ae7c2526dc484a76687f56f29581
Dan Neve <6909667+DanForever@users.noreply.github.com>
2021-12-21 21:39:43 +0100

    Little popup window to detail the current device connections to the user
    Appears when a user left clicks on the system tray icon.
    
    Very much WIP

--- Windows/Frontend/App.xaml.cs
+++ Windows/Frontend/App.xaml.cs
@@ -6,17 +6,19 @@ namespace HardwareMonitor
 {
 	public partial class App : Application
 	{
+		public static App Application => (App)Current;
+
 		private TaskbarIcon _tb;
 
 		public Main Program { get; set; }
 
 		private async void Application_Startup(object sender, StartupEventArgs e)
 		{
-			//initialize NotifyIcon
-			_tb = (TaskbarIcon)FindResource("MyNotifyIcon");
-
 			// Instanticate the class that will perform the main body of work for us
 			Program = new Main();
+
+			//initialize NotifyIcon
+			_tb = (TaskbarIcon)FindResource("MyNotifyIcon");
 
 			// Run the application
 			int retcode = await Program.Run();
---
+++ Windows/Frontend/QuickInfo/QuickInfo.xaml.cs
@@ -0,0 +1,63 @@
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HardwareMonitor.QuickInfo
+{
+	public class DeviceInfo : INotifyPropertyChanged
+	{
+		private Device _device;
+
+		public string Port => _device.Connection.Name;
+
+		public DeviceInfo(Device device)
+		{
+			_device = device;
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+	}
+
+	public partial class QuickInfo : UserControl
+	{
+		public ObservableCollection<DeviceInfo> Devices
+		{
+			get => (ObservableCollection<DeviceInfo>)GetValue(DevicesProperty);
+			private set => SetValue(DevicesProperty, value);
+		}
+
+		public static readonly DependencyProperty DevicesProperty = DependencyProperty.Register(nameof(Devices), typeof(ObservableCollection<DeviceInfo>), typeof(QuickInfo));
+
+		public QuickInfo()
+		{
+			InitializeComponent();
+			PopulateDeviceInfo();
+
+			App.Application.Program.DeviceConnected += Program_DeviceConnected;
+			App.Application.Program.DeviceDisconnected += Program_DeviceDisconnected;
+
+			Devices = new ObservableCollection<DeviceInfo>();
+		}
+
+		private void Program_DeviceConnected(Device device)
+		{
+			Devices.Add(new DeviceInfo(device));
+		}
+
+		private void Program_DeviceDisconnected(Device device)
+		{
+			DeviceInfo deviceInfo = Devices.First(d => d.Port == device.Connection.Name);
+			Devices.Remove(deviceInfo);
+		}
+
+		private void PopulateDeviceInfo()
+		{
+			foreach(Device device in App.Application.Program.Devices)
+			{
+				//Devices.Add(new DeviceInfo(device));
+			}
+		}
+	}
+}

53c1c9ef7bebb824ae0add13041d4b4740f67659
Dan Neve <6909667+DanForever@users.noreply.github.com>
2022-09-01 22:51:41 +0200

    Replace magic number with define

--- Device/src/Screen/Arduino_GFX_Wrapper.cpp
+++ Device/src/Screen/Arduino_GFX_Wrapper.cpp
@@ -22,7 +22,7 @@
 template<>
 ArduinoGfx<Arduino_HWSPI, Arduino_ILI9486_18bit>::ArduinoGfx()
 	: m_bus(SCREEN_TFT_DC /* DC */, SCREEN_TFT_CS /* CS */)
-	, m_gfx(&m_bus, 19 /* RST */, 0 /* rotation */, false /* IPS */)
+	, m_gfx(&m_bus, SCREEN_TFT_RST /* RST */, 0 /* rotation */, false /* IPS */)
 {
 }
 
